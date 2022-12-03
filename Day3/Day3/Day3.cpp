6e4a91d1400aafdacec64387d3e248ca247ee202
rickyrnt <rick.yarnot.255@gmail.com>
2022-12-03 16:45:59 -0500

    Day3 solution

--- Day3/Day3/Day3.cpp
+++ Day3/Day3/Day3.cpp
@@ -9,30 +9,59 @@
 
 using namespace std;
 
+int getVal(char a);
+
 int main()
 {
     int score = 0;
     ifstream input("input.txt");
-    string line;
+    string line, ln2, ln3;
 
     if (input.is_open()) {
+        //part two
+
+    funny:
+        while (getline(input, line)) {
+            getline(input, ln2);
+            getline(input, ln3);
+
+            while (true) {
+                for (int i = 0; i < line.length(); i++) {
+                    for (int j = 0; j < ln2.length(); j++) {
+                        if (line[i] == ln2[j]) {
+                            for (int k = 0; k < ln3.length(); k++) {
+                                if (ln3[k] == line[i]) {
+                                    score += getVal(line[i]);
+                                    goto funny;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        cout << score;
+
+        /*part one
+    funny:
         while (getline(input, line)) {
             //O(n^2). Cry about it.
-        funny:
             for (int i = 0; i < line.length() / 2; i++) {
                 for (int j = line.length() / 2; j < line.length(); j++) {
                     if (line[i] == line[j]) {
-                        cout << line[i] << " aka " << getVal(line[i]) << "\n";
+                        score += getVal(line[i]);
                         goto funny;
                     }
                 }
             }
         }
-    }
+        cout << score;
+        */
+    } else { cout << "input not open!"; }
 }
 
 int getVal(char a) {
     if (a >= 97) {
-        return 96 - a;
-    } else return 92 - a;
+        return a - 96;
+    } else return a - 38;
 }
